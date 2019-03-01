3cf56c1f76d0b3ea8e9918ff251a1f80ce2c9734
Kevin Padilla <kevin.p@digitalroominc.com>
2019-03-01 01:50:38 -0800

    comments

--- core/controllers/book.controller.ts
+++ core/controllers/book.controller.ts
@@ -11,22 +11,42 @@ export class BookController {
 
     constructor() {}
 
+    /**
+     * Returns a book collection
+     */
     getBooks(): Promise<Book[]> {
         return this.BookService.getBooks();
     }
 
+    /**
+     * Returns a book record
+     * @param bookId 
+     */
     getBook(bookId: number): Promise<Book> {
         return this.BookService.getBook(bookId);
     }
 
+    /**
+     * Save new book record
+     * @param bookData 
+     */
     createBook(bookData : any): Promise<InsertResult> {
         return this.BookService.createBook(bookData);
     }
 
+    /**
+     * Update a book record
+     * @param bookId 
+     * @param bookData 
+     */
     updateBook(bookId: number, bookData: Request): Promise<UpdateResult> {
         return this.BookService.updateBook(bookId, bookData);
     }
 
+    /**
+     * Delete a book record
+     * @param bookId 
+     */
     deleteBook(bookId: number): Promise<DeleteResult> {
         return this.BookService.deleteBook(bookId);
     }
