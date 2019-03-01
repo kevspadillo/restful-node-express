7662849ee8b7f0ca747ff9ff46a72c163b1893cd
Kevin Padilla <kevin.p@digitalroominc.com>
2019-03-01 01:50:23 -0800

    comments

--- core/services/book.service.ts
+++ core/services/book.service.ts
@@ -12,12 +12,19 @@ export class BookService {
         
     }
     
+    /**
+     * Connects to the database and fetches books
+     */
     public async getBooks() : Promise<Book[]> {
         return this.DB.connect().then((connection) => {
             return connection.manager.find(Book);
         })
     }
 
+    /**
+     * Connects to the database and fetch a book record
+     * @param bookId 
+     */
     public getBook(bookId : number) : Promise<any> {
         return this.DB.connect().then((connection) => {
             return connection
@@ -29,6 +36,11 @@ export class BookService {
         })
     }
 
+
+    /**
+     * Connects to the database and saves a new book record
+     * @param bookData 
+     */
     public createBook(bookData: any) : Promise<InsertResult> {
 
         const book = {title : bookData.title };
@@ -43,6 +55,11 @@ export class BookService {
         })
     }
 
+    /**
+     * Connects to the database and updates a book record based on the provided book id
+     * @param bookId 
+     * @param bookData 
+     */
     public updateBook(bookId: number, bookData: any) : Promise<UpdateResult> {
 
         const book = {title : bookData.title };
@@ -56,6 +73,10 @@ export class BookService {
         })
     }
 
+    /**
+     * Connects to the database and deletes a book record
+     * @param bookId 
+     */
     public deleteBook(bookId) : Promise<DeleteResult> {
         return this.DB.connect().then((connection) => {
             return connection.createQueryBuilder()
